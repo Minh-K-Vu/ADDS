d6b9e9b56b683804cec27b829416e1b62cb16df9
Minh <minh.k.vu@student.adelaide.edu.au>
2025-03-13 19:49:37 +1030

    chicken

--- Referee.cpp
+++ Referee.cpp
@@ -6,12 +6,14 @@ Player* Referee::refGame(Player* player1, Player* player2) {
     Move* move1 = player1->makeMove();
     Move* move2 = player2->makeMove();
     
-    if (move1->beats(move2)){
+    if (move1->beats(move2)) {
       std::cout << player1->getName() << " wins!\n";
       return player1;
-    } else if (move2->beats(move1)) {
+  } else if (move2->beats(move1)) {
       std::cout << player2->getName() << " wins!\n";
       return player2;
-    }
-    return player2;
+  } else {
+      std::cout << "It's a tie!\n";
+      return nullptr;
+  }
 }
